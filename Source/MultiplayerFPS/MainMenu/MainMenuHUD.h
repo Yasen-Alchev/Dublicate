f34c66f9be99ccd5c40da4022083de1d515ad208
AlexHadzhiev <72384702+AlexHadzhiev@users.noreply.github.com>
2022-03-17 21:30:48 +0200

    Update MainMenuHUD.h

--- Source/MultiplayerFPS/MainMenu/MainMenuHUD.h
+++ Source/MultiplayerFPS/MainMenu/MainMenuHUD.h
@@ -1,19 +1,3 @@
-/*  Copyright (C) 2001-2003 Free Software Foundation, Inc.
-
-   MultiplayerFPS is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   MultiplayerFPS is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with MultiplayerFPS.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -21,9 +5,6 @@
 #include "Components/WidgetComponent.h"
 #include "MainMenuHUD.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class MULTIPLAYERFPS_API AMainMenuHUD : public AHUD
 {
